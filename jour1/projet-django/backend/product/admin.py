9ad80469c20f61527dbb9e95dfe860fdbcd73418
Awadi Bedja <abedja@picoxia.com>
2026-08-03 17:11:25 +0200

    Enregistrement de Category et Product dans l'admin

--- jour1/projet-django/backend/product/admin.py
+++ jour1/projet-django/backend/product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "created_at")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
