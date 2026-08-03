33b5764313d0698c30b8d308fb948a5d9977a8d6
Awadi Bedja <abedja@picoxia.com>
2026-08-03 17:29:39 +0200

    Ajout des filtres categorie et prix sur la liste des produits

--- jour1/projet-django/backend/product/views.py
+++ jour1/projet-django/backend/product/views.py
@@ -1,5 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 
+from .filters import ProductFilter
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -7,6 +9,8 @@ from .serializers import ProductSerializer
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.select_related("category").all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
 
 class ProductDetailView(generics.RetrieveAPIView):
